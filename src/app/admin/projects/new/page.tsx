ac5eef2c53632f740b2e2653207fa705016e9421
Namika <loud.music.n@gmail.com>
2025-06-11 15:46:27 +0900

    新規登録・編集画面の作成_ユーザー・管理者(#TSK-206)

--- src/app/admin/projects/new/page.tsx
+++ src/app/admin/projects/new/page.tsx
@@ -1,3 +1,13 @@
+import { Title } from '@mantine/core'
+import { InputForm } from '@/app/admin/projects/_component/InputForm'
+
 export default function NewProject() {
-  return <>プロジェクト新規作成ページ</>
+  return (
+    <>
+      <Title order={2} ta="center" mb="lg">
+        新規案件作成
+      </Title>
+      <InputForm pageType="NEW" />
+    </>
+  )
 }
