cdb04ecc608973e30e932064ee4da37778787baa
Namika <loud.music.n@gmail.com>
2025-06-17 16:34:48 +0900

    feat:ユーザー案件詳細画面でのユーザー認証追加(#TSK-305)

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,8 @@
 import { ProjectDetails } from '@/app/projects/[projectId]/_component/ProjectDetails'
+import { RoleType } from '@/types'
 import { Box, Button, Title } from '@mantine/core'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import { serverApi } from '~/lib/trpc/server-api'
 
 export default async function ProjectDetail({
@@ -10,6 +12,14 @@ export default async function ProjectDetail({
 }) {
   const projectId = params.projectId
   const api = serverApi()
+
+  // ユーザー認証
+  const user = await api.userInfo().catch(() => redirect('/'))
+  if (!user) redirect('/')
+
+  // 管理者権限でのログインの場合はリダイレクト
+  const userData = await api.user.find(user.id)
+  if (userData?.role === RoleType.ADMIN) redirect('/')
 
   // プロジェクトの取得
   const project = await api.projects.find(projectId).catch(() => null)
