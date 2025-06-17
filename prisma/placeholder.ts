ab75357a99b21319463c07c5ad6828315b39c4d8
Namika <loud.music.n@gmail.com>
2025-06-17 15:00:30 +0900

    feat:prismaの更新(#TSK-302)

--- prisma/placeholder.ts
+++ prisma/placeholder.ts
@@ -2,34 +2,38 @@ import { Role } from '@prisma/client'
 
 export const users = [
   {
+    id: '4cf624a5-7b99-423d-8cba-303a56f3602e',
     username: '山田 太郎',
     email: 'yamadataro@example.com',
-    password: 'password',
-    role: Role.USER
+    role: Role.USER,
+    password: 'password123'
   },
   {
+    id: 'fb6fc9c6-7016-4bfc-a44c-7ff59aa72ef0',
     username: '伊藤 直樹',
     email: 'itonaoki@example.com',
-    password: 'password',
-    role: Role.USER
+    role: Role.ADMIN,
+    password: 'password123'
   },
   {
+    id: '64e27b63-2cb3-4569-873b-10f66d6b440a',
     username: '鈴木花子',
     email: 'suzukihanako@example.com',
-    password: 'password',
-    role: Role.USER
+    role: Role.USER,
+    password: 'password123'
   },
   {
+    id: 'bb669aa9-d3d4-417b-85b9-1d58acfe5e81',
     username: '高橋 結衣',
     email: 'takahashiyui@example.com',
-    password: 'password',
-    role: Role.USER
+    role: Role.USER,
+    password: 'password123'
   },
   {
     username: '中村 美咲',
     email: 'nakamuramisaki@example.com',
-    password: 'password',
-    role: Role.USER
+    role: Role.ADMIN,
+    password: 'password123'
   }
 ]
 
