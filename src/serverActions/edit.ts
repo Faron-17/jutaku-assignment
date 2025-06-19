0e3070e88d45339f0417ce2e184260b30b6c0d3b
Namika <loud.music.n@gmail.com>
2025-06-19 17:11:51 +0900

    fix:型定義を既存呼び出しに変更(#TSK-315)

--- src/serverActions/edit.ts
+++ src/serverActions/edit.ts
@@ -1,17 +1,9 @@
 'use server'
 
+import type { Projects } from '@prisma/client'
 import { serverApi } from '~/lib/trpc/server-api'
 
-type UpdateProjectInput = {
-  id: string
-  title: string
-  summary: string
-  skills: string[]
-  rate: number
-  deadlineAt: Date
-}
-
-export async function editProject(data: UpdateProjectInput) {
+export async function editProject(data: Projects) {
   const api = serverApi()
   return api.projects.update(data)
 }
